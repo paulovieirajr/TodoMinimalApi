2f0cc21c7844500177ab3d619f734ccd05eb89c7
Paulo Vieira <80427803+vieirajunior-90@users.noreply.github.com>
2023-09-24 19:12:54 -0300

    Creating endpoints GET and POST

--- TodoMinimalApi/Program.cs
+++ TodoMinimalApi/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using TodoMinimalApi.Context;
+using TodoMinimalApi.Dtos;
+using TodoMinimalApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,5 +21,25 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.MapGet("/tasks", async (AppDbContext context) =>
+{
+    var tasks = await context.Tasks.ToListAsync();
+    return tasks.Count > 0 ? Results.Ok(tasks) : Results.NotFound();
+});
+
+app.MapGet("/tasks/{id}", async (AppDbContext context, int id) =>
+{
+    var task = await context.Tasks.FindAsync(id);
+    return task is not null ? Results.Ok(task) : Results.NotFound();
+});
+
+app.MapPost("/tasks", async (AppDbContext context, TaskDto taskDto) =>
+    {
+        var task = new TaskModel(taskDto.Title, false);
+        await context.Tasks.AddAsync(task);
+        await context.SaveChangesAsync();
+        return Results.Created($"/tasks/{task.Id}", task);
+    });
+
+
 app.Run();

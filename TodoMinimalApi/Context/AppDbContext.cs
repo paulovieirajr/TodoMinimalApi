84744de914163c69d0e28f629032490cb4100040
Paulo Vieira <80427803+vieirajunior-90@users.noreply.github.com>
2023-09-24 18:56:36 -0300

    Creating DbContext

---
+++ TodoMinimalApi/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task = TodoMinimalApi.Models.Task;
+
+namespace TodoMinimalApi.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Task> Tasks => Set<Task>();
+}

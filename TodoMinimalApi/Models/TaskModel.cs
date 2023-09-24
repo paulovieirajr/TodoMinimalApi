f176b09ec1cbdde659032778cfa81536addd6da5
Paulo Vieira <80427803+vieirajunior-90@users.noreply.github.com>
2023-09-24 19:11:55 -0300

    Rename Task to TaskModel

---
+++ TodoMinimalApi/Models/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TodoMinimalApi.Models;
+
+public class TaskModel
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public bool IsCompleted { get; set; }
+
+    public TaskModel(string? title, bool isCompleted)
+    {
+        Title = title;
+        IsCompleted = isCompleted;
+    }
+}
